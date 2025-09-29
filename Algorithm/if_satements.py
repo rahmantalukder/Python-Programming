78bcc768e0c9eff6527a1ac6e1596c002f216a9e
rahman_talukder <comt-210621@dti.ac>
2025-09-29 23:43:11 +0600

    if

---
+++ Algorithm/if_satements.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+
+if age> 18:
+    print("You art not a child")
+
+elif age> 18:
+    print("You art not a child")
+elif age> 18:
+    print("You art not a child")
+else:
+    print("You are a bccha")
+
